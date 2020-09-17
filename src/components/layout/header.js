43f4e98afc4b06b09569c3d113b69bba3c226c26
Alejo <alejogarb25@gmail.com>
2020-09-17 11:41:01 -0300

    Create header.js

---
+++ src/components/layout/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function header() {
+    const headerStyle = {
+        background: "black",
+        color: "white",
+    }
+    return (
+        <header style={headerStyle}>
+            <h1>Todo List</h1>
+        </header>
+    )
+}
+
+
+
