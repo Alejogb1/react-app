06222d1c4b25a132ad85cbe61594ed71f9d557ef
Alejo <alejogarb25@gmail.com>
2020-09-17 09:12:56 -0300

    Update Todos.js

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,15 +1,25 @@
 import React,{Component} from 'react';
 import "./App.css";
 import { render } from '@testing-library/react';
-
+import {Todoitem} from "./Todoitem";
+import {PropTypes} from "prop-types";
 
 export default class Todos extends Component {
+        markComplete = () => {
+                        console.log("hello")
+
+        }
     render () {
             return this.props.todos.map((todo)=>
-                    <h3>{todo.title}</h3>
+                <Todoitem key={todo.id} todo={todo} markComplete={this.markComplete}/>
             )
           
     }
+}
+
+// PropTypes
+Todos.propTypes = {
+       todos: PropTypes.array.isRequired   
 }
 
 
